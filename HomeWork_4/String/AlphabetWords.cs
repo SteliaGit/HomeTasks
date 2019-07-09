35a7cb98770717e93a154240ce62ddbe4186fe8b
SteliaGit <SteliaGit@users.noreply.github.com>
2019-07-10 01:27:03 +0300

    Add in project Work with strings -  New functionality (Usin FileSystemWatcher and Multithreading)

---
+++ HomeWork_4/String/AlphabetWords.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String
+{
+    class AlphabetWords
+    {
+        public static int countWord = 0;
+        
+        public void GetWordsAlphabet()
+        {   
+            Repository repository = new Repository();
+            Sentences sentences = new Sentences();
+            sentences.GetSenteces();
+            if (Word.word.Count > 0)
+            {
+               Word.word.Sort();
+               var dis = Word.word.Distinct();
+               countWord = dis.Count();
+                foreach (var str in dis)
+                {                
+                   int count = Word.word.Count(x => x == str);
+                   repository.SaveWord(str, count);                  
+                }
+            }
+        }
+    }
+}
---
+++ HomeWork_4/String/ClearText.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace String
+{
+    class ClearText
+    {     
+        public static string a;
+        public void GetClearString(string patch)
+        {
+            Repository repository = new Repository();
+            repository.LoadString(patch);
+
+            StringBuilder sb = new StringBuilder(Repository.line);
+            a = sb
+              .Replace("----", "").Replace("--", "-").Replace("....",".").Replace('"',' ').Replace("\n\n", "\n").Replace("\n\n\n", "\n").Replace("\n\n\n\n", "\n")
+              .Replace("(", "").Replace(")", "").Replace("[", "").Replace("]", "").Replace("{", "").Replace("}", "").Replace("_", "").Replace("  ", "")
+              .Replace("=","").Replace("+", "").Replace("|", "").Replace(":", " ").Replace(";", " ").Replace("\n\n\n\n\n", "\n").Replace("#","")
+              .Replace("<","").Replace(">","").Replace("=-", "").Replace("*","").Replace(" '"," ").Replace("' ", " ").ToString();
+        // Чистку можно было сделать более подробную, также через регулярку было бы проще но я здесь отразил работу замены в строке...
+        }
+
+    }
+}
